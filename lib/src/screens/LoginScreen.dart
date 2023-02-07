bca929c6bf47c9699423cfb2290394911ba77925
Rohit Pakhrin <roitpak@gmail.com>
2023-02-07 08:57:11 +0545

    navigation setup

---
+++ lib/src/screens/LoginScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreen();
+}
+
+class _LoginScreen extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/dashboard');
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/MainScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_flutter_application/src/screens/Dashboard.dart';
+import 'package:my_first_flutter_application/src/screens/LoginScreen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      title: "News!",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/dashboard': (context) => const Dashboard(),
+      },
+    );
+  }
+}
