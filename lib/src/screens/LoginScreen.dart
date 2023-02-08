0aac5c0928b88ecbc956593a28921bc0f38de701
Rohit Pakhrin <roitpak@gmail.com>
2023-02-08 08:44:48 +0545

    design login screen

--- lib/src/screens/LoginScreen.dart
+++ lib/src/screens/LoginScreen.dart
@@ -8,20 +8,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
+  var username = "";
+  var password = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/dashboard');
-          },
-          child: const Text('Launch screen'),
+        appBar: AppBar(
+          title: const Text('First Screen'),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: TextField(
+                onChanged: (value) => {
+                  setState(() {
+                    username = value;
+                  })
+                },
+                decoration: const InputDecoration(
+                    labelText: "Username",
+                    hintText: "user123",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              child: TextField(
+                onChanged: (value) => {
+                  setState(() {
+                    password = value;
+                  })
+                },
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "*********",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print("Hello world" + username + password);
+                },
+                child: const Text("Login"))
+          ],
+        ));
   }
 }
