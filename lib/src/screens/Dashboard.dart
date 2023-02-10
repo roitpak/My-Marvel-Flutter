85a0e26c084d6c9acf62ba001098d3121cfef9cc
Rohit Pakhrin <roitpak@gmail.com>
2023-02-10 15:26:33 +0545

    validate login and sign out

--- lib/src/screens/Dashboard.dart
+++ lib/src/screens/Dashboard.dart
@@ -1,20 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_application/auth.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
+
+  Future<void> signOut(BuildContext context) async {
+    await Auth().signOut();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        Navigator.pop(context);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Screen'),
+        title: const Text('Dashboard'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pop(context);
+            signOut(context);
+            // Navigator.pop(context);
           },
-          child: const Text('Go back!'),
+          child: const Text('Logout'),
         ),
       ),
     );
--- lib/src/screens/LoginScreen.dart
+++ lib/src/screens/LoginScreen.dart
@@ -12,10 +12,11 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreen extends State<LoginScreen> {
   var email = "";
   var password = "";
+  var emailError = "";
+  var passwordError = "";
+  var loading = false;
+
   final User? user = Auth().currentUser;
-  Future<void> signOut() async {
-    await Auth().signOut();
-  }
 
   Future<void> signInWithEmailAndPassword() async {
     try {
@@ -32,25 +33,54 @@ class _LoginScreen extends State<LoginScreen> {
   }
 
   void checkLogin() async {
-    try {
-      await Auth().signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      print(e.message);
-      // setState(() {
-      //   errorMessage = e.message;
-      // });
-    }
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-      } else {
-        Navigator.pushNamed(context, '/dashboard');
-        print('User is signed in!');
+    if (validate()) {
+      setState(() {
+        loading = true;
+      });
+      try {
+        await Auth().signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } on FirebaseAuthException catch (e) {
+        setState(() {
+          loading = false;
+        });
+        print(e.message);
+        setState(() {
+          emailError = e.message!;
+        });
       }
-    });
+      setState(() {
+        loading = false;
+      });
+      FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null) {
+          print('User is currently signed out!');
+        } else {
+          Navigator.pushNamed(context, '/dashboard');
+          print('User is signed in!');
+        }
+      });
+    }
+  }
+
+  bool validate() {
+    if (!RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email)) {
+      setState(() {
+        emailError = "Email is not a valid email";
+      });
+      return false;
+    }
+    if (password.length < 8) {
+      setState(() {
+        passwordError = "Password must be at least 8 characters";
+      });
+      return false;
+    }
+    return true;
   }
 
   @override
@@ -70,13 +100,15 @@ class _LoginScreen extends State<LoginScreen> {
               child: TextField(
                 onChanged: (value) => {
                   setState(() {
+                    emailError = "";
                     email = value;
                   })
                 },
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
+                    errorText: emailError == "" ? null : emailError,
                     labelText: "Email",
                     hintText: "email@email.com",
-                    border: OutlineInputBorder()),
+                    border: const OutlineInputBorder()),
               ),
             ),
             Padding(
@@ -85,20 +117,24 @@ class _LoginScreen extends State<LoginScreen> {
                 onChanged: (value) => {
                   setState(() {
                     password = value;
+                    passwordError = "";
                   })
                 },
                 obscureText: true,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
+                    errorText: passwordError == "" ? null : passwordError,
                     labelText: "Password",
                     hintText: "*********",
-                    border: OutlineInputBorder()),
+                    border: const OutlineInputBorder()),
               ),
             ),
             ElevatedButton(
                 onPressed: () {
                   checkLogin();
                 },
-                child: const Text("Login"))
+                child: loading
+                    ? const CircularProgressIndicator()
+                    : const Text("Login"))
           ],
         ));
   }
